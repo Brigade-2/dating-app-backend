4727d9a56e942ee8f52fd62eec989d6f70f2415e
Fedor Anikeev <fedos3d@gmail.com>
2022-10-17 16:46:10 +0300

    init

---
+++ src/main/java/org/openapitools/model/Match.java
@@ -0,0 +1,95 @@
+package org.openapitools.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.ArrayList;
+import java.util.List;
+import org.openapitools.model.Message;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import javax.annotation.Generated;
+
+/**
+ * Match
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-10-17T13:35:38.732438Z[Etc/UTC]")
+public class Match {
+
+  @JsonProperty("messages")
+  @Valid
+  private List<Message> messages = null;
+
+  public Match messages(List<Message> messages) {
+    this.messages = messages;
+    return this;
+  }
+
+  public Match addMessagesItem(Message messagesItem) {
+    if (this.messages == null) {
+      this.messages = new ArrayList<>();
+    }
+    this.messages.add(messagesItem);
+    return this;
+  }
+
+  /**
+   * Get messages
+   * @return messages
+  */
+  @Valid 
+  @Schema(name = "messages", required = false)
+  public List<Message> getMessages() {
+    return messages;
+  }
+
+  public void setMessages(List<Message> messages) {
+    this.messages = messages;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Match match = (Match) o;
+    return Objects.equals(this.messages, match.messages);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(messages);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Match {\n");
+    sb.append("    messages: ").append(toIndentedString(messages)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ src/main/java/org/openapitools/model/Message.java
@@ -0,0 +1,179 @@
+package org.openapitools.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import javax.annotation.Generated;
+
+/**
+ * Message
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-10-17T13:37:18.633119Z[Etc/UTC]")
+public class Message {
+
+  @JsonProperty("fromId")
+  private Long fromId;
+
+  @JsonProperty("toId")
+  private Long toId;
+
+  @JsonProperty("timestamp")
+  private Long timestamp;
+
+  @JsonProperty("text")
+  private String text;
+
+  @JsonProperty("viewed")
+  private Boolean viewed;
+
+  public Message fromId(Long fromId) {
+    this.fromId = fromId;
+    return this;
+  }
+
+  /**
+   * Get fromId
+   * @return fromId
+  */
+  
+  @Schema(name = "fromId", required = false)
+  public Long getFromId() {
+    return fromId;
+  }
+
+  public void setFromId(Long fromId) {
+    this.fromId = fromId;
+  }
+
+  public Message toId(Long toId) {
+    this.toId = toId;
+    return this;
+  }
+
+  /**
+   * Get toId
+   * @return toId
+  */
+  
+  @Schema(name = "toId", required = false)
+  public Long getToId() {
+    return toId;
+  }
+
+  public void setToId(Long toId) {
+    this.toId = toId;
+  }
+
+  public Message timestamp(Long timestamp) {
+    this.timestamp = timestamp;
+    return this;
+  }
+
+  /**
+   * Get timestamp
+   * @return timestamp
+  */
+  
+  @Schema(name = "timestamp", required = false)
+  public Long getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(Long timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public Message text(String text) {
+    this.text = text;
+    return this;
+  }
+
+  /**
+   * Get text
+   * @return text
+  */
+  
+  @Schema(name = "text", required = false)
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  public Message viewed(Boolean viewed) {
+    this.viewed = viewed;
+    return this;
+  }
+
+  /**
+   * Get viewed
+   * @return viewed
+  */
+  
+  @Schema(name = "viewed", required = false)
+  public Boolean getViewed() {
+    return viewed;
+  }
+
+  public void setViewed(Boolean viewed) {
+    this.viewed = viewed;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Message message = (Message) o;
+    return Objects.equals(this.fromId, message.fromId) &&
+        Objects.equals(this.toId, message.toId) &&
+        Objects.equals(this.timestamp, message.timestamp) &&
+        Objects.equals(this.text, message.text) &&
+        Objects.equals(this.viewed, message.viewed);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(fromId, toId, timestamp, text, viewed);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Message {\n");
+    sb.append("    fromId: ").append(toIndentedString(fromId)).append("\n");
+    sb.append("    toId: ").append(toIndentedString(toId)).append("\n");
+    sb.append("    timestamp: ").append(toIndentedString(timestamp)).append("\n");
+    sb.append("    text: ").append(toIndentedString(text)).append("\n");
+    sb.append("    viewed: ").append(toIndentedString(viewed)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
