463bbc7584b63c9b20b7f68e253e2c915408ed38
fedos3d <fedos3d@gmail.com>
2022-11-29 12:55:53 +0000

    cors attmep2 fix

--- src/main/java/org/tinder/configuration/WebConfig.java
+++ src/main/java/org/tinder/configuration/WebConfig.java
@@ -1,11 +1,13 @@
 package org.tinder.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig{
+public class WebConfig implements WebMvcConfigurer {
     // enable webmvc
     @Override
     public void addCorsMappings(CorsRegistry registry) {
